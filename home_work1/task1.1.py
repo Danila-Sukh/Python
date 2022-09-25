8dfc6128f1333b2bee2b105fdff2d1ab9d10d15d
Danila Sukhorukov <suhorukovdanila@yandex.ru>
2022-09-25 15:51:25 +0300

    first commit

---
+++ home_work1/task1.1.py
@@ -0,0 +1,7 @@
+a = int(input())
+if a == 6 or a == 7:
+    print("Выходной")
+elif a > 0 and a < 7:
+    print("Будний")
+else:
+    print("Неверное число")
---
+++ home_work1/task3.2.py
@@ -0,0 +1,25 @@
+def input_numbers(x):
+    xy = ["x", "y"]
+    a = []
+    for i in range(x):
+        reality = False
+        while not reality:
+            try:
+                number = int(input(f"Введите координату {xy[i]}: "))
+                a.append(number)
+                reality = True
+            except ValueError:
+                print("Ошибка")
+    return a
+
+
+def Length(a, b):
+    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return lengthSegment
+
+
+print("Введите координаты точки А")
+A = input_numbers(2)
+print("Введите координаты точки В")
+B = input_numbers(2)
+print(f"Длина отрезка: {round(Length(A, B),2)}")
